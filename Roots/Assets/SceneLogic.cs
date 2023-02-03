1e34b1d8c0c027ea6164ec4a99a0357b3ae2f4cb
st4g20 <st4g20@soton.ac.uk>
2023-02-03 23:37:01 +0000

    Test Add

---
+++ Roots/Assets/SceneLogic.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLogic : MonoBehaviour
+{
+    [SerializeField] GameObject camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
