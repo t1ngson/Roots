9bcd5657dbd4ce3066cd2257577838691d1f284c
Rudra <rmutalik@outlook.com>
2023-02-04 00:53:41 +0000

    unity sucks

--- Roots/Assets/scripts/WorldGen.cs
+++ Roots/Assets/scripts/WorldGen.cs
@@ -4,23 +4,29 @@ using UnityEngine;
 
 public class WorldGen : MonoBehaviour
 {
-
+	
+	public int waterGathered = 0;
+	public int nutrientGathered = 0;
+	
+	
     public GameObject nutrient;
+	public GameObject water;
 
     private int count;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+		
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (count++ == 20)
+        if (count++ % 20 == 0)
         {
-            Instantiate(nutrient, new Vector3(0, 0, 0), Quaternion.identity);
+            Instantiate(nutrient, new Vector3(0, -1, 0), Quaternion.identity);
+			Instantiate(water, new Vector3(0, 0, 1), Quaternion.identity);
         }
     }
 }
