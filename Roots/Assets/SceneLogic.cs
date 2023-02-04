ea51e580ac9d7045de74f6204bdcee97b8b76dfe
st4g20 <st4g20@soton.ac.uk>
2023-02-04 00:54:01 +0000

    First stuff

--- Roots/Assets/SceneLogic.cs
+++ Roots/Assets/SceneLogic.cs
@@ -6,15 +6,19 @@ public class SceneLogic : MonoBehaviour
 {
     [SerializeField] GameObject camera;
 
+    void Awake(){
+
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         camera.transform.position = camera.transform.position + new Vector3(0,-1, 0);
     }
 }
