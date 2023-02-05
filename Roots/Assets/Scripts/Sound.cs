aa75c4122c5d6f0bc00cfc1d9dd8ee686d6dd44f
st4g20 <st4g20@soton.ac.uk>
2023-02-05 03:55:25 +0000

    Sound effects added

--- Roots/Assets/Scripts/Sound.cs
+++ Roots/Assets/Scripts/Sound.cs
@@ -6,15 +6,24 @@ public class Sound : MonoBehaviour
 {
 
     public AudioSource adSource;
+    public AudioSource deathSound;
+    public AudioSource splash;
+    public AudioSource sparkle;
+    public AudioSource whack;
+    public AudioSource nom; 
 
     void Start()
     {
-        adSource.Play();        
+        adSource.Play();   
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void playSound(AudioSource audioSource){
+        audioSource.Play();
     }
 }
