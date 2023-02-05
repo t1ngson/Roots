871ec2bcbe5bb1a8c6da829a12b73546ef5f05dd
D4nDude <hobbs.daniel.007@gmail.com>
2023-02-05 15:27:57 +0000

    fixes

--- Roots/Assets/Paths/PathEditor.cs
+++ Roots/Assets/Paths/PathEditor.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -63,3 +63,4 @@ public class PathEditor : Editor
         path = creator.path;
     }
 }
+*/
