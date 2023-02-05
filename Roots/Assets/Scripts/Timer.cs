3252eb6fddfb53bb237cd99a435cd7c205278cbe
HannoAtomic <theepicest2@gmail.com>
2023-02-05 14:05:33 +0000

    Implemented basic timer functionality Changed some positioning of text to fit timer

---
+++ Roots/Assets/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeValue = 30;
+    public Text timerText;
+
+    void Update()
+    {
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue = 0;
+            //SceneManager.LoadScene("End Scene");
+        }
+        DisplayTime(timeValue);
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        if (timeToDisplay < 0)
+        {
+            timeToDisplay = 0;
+        }
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
