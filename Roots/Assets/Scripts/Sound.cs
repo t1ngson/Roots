f50a3eed4d5d1f21006d28c4e632e50d43964e71
st4g20 <st4g20@soton.ac.uk>
2023-02-05 02:58:10 +0000

    Music Added

---
+++ Roots/Assets/Scripts/Sound.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+
+    public AudioSource adSource;
+
+    void Start()
+    {
+        adSource.Play();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
